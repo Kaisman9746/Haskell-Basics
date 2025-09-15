99369a83e6fc91a01d6be76eb4f302f8abdd910a
Kaisman9746 <97808078+Kaisman9746@users.noreply.github.com>
2025-09-15 17:00:24 +0100

    Update Chapter1.hs

--- Pratical-task/Chapter1.hs
+++ Pratical-task/Chapter1.hs
@@ -1,1 +1,15 @@
+double :: Int -> Int 
+double x = x * 2
 
+main :: IO () 
+main = do 
+ print $ double 10 
+ --answer = 20
+
+ increment :: Int -> Int 
+increment x = x + 1
+
+main :: IO ()
+main = do 
+ print $ increment 15
+  --answer = 16
