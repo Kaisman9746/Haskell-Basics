a357f859bccbc2738d73e89822acd19d67631307
Kaisman9746 <97808078+Kaisman9746@users.noreply.github.com>
2025-09-22 15:25:45 +0100

    Update Chapter2.hs

--- Pratical-task/Chapter2.hs
+++ Pratical-task/Chapter2.hs
@@ -138,4 +138,22 @@ main :: IO ()
 main = do 
  print $ isHaskellFun
   --Outcome = True 
+Use prefix notation for the following infix expressions:
+5 + 3, 10 * 4, True && False. 
 
+prefix :: Int -> Int 
+prefix x = x
+
+main :: IO ()
+main = do 
+ print $ (+) 5 3
+  --output = 8
+ 
+
+prefix :: Int -> Int 
+prefix x = x 
+
+main :: IO ()
+main = do 
+ print $ (*) 10  4
+ --Output = 40 
