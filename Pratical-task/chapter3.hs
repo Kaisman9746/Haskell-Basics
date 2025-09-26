2bfc9694e2a58d51a8940b73d8c94a0432086efe
Kaisman9746 <97808078+Kaisman9746@users.noreply.github.com>
2025-09-26 14:53:09 +0100

    Update chapter3.hs

--- Pratical-task/chapter3.hs
+++ Pratical-task/chapter3.hs
@@ -1,3 +1,36 @@
+HC3T1 - Task 1: Check if a number is positive, negative, or zero
+Define a function checkNumber :: Int -> String.
+Use an if-then-else statement to check if the number is positive, negative, or zero.
+Return "Positive", "Negative", or "Zero" accordingly.
+Test your function with checkNumber 5, checkNumber (-3), and checkNumber 0.
+
+grade :: Int -> String
+grade x = 
+ if x >= 90 && x <= 99
+  then "A"
+      else if x >= 80 && x <= 89
+         then "B"
+            else if x >= 70 && x <= 79
+               then "C"
+                  else if x >= 60 && x <= 69
+                     then "D"
+                        else if x >= 50 && x <= 60 
+                           then "F"
+                              else "invalid grade"
+main :: IO ()
+main = do 
+ putStrLn (grade 95) 
+ putStrLn (grade 75)
+ putStrLn (grade 50)
+ putStrLn (grade 81)
+ putStrLn (grade 68)
+ putStrLn (grade 30)
+ putStrLn (grade 58) 
+
+
+
+
+
 grade :: Int -> [Char]
 grade x 
  | x >= 90 && x <= 99 = "A"
