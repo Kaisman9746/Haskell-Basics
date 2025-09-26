8d8969746a202319b57a27d9047bd2db3786a14d
Kaisman9746 <97808078+Kaisman9746@users.noreply.github.com>
2025-09-26 14:39:47 +0100

    Update chapter3.hs

--- Pratical-task/chapter3.hs
+++ Pratical-task/chapter3.hs
@@ -1,1 +1,14 @@
-main 
+grade :: Int -> [Char]
+grade x 
+ | x >= 90 && x <= 99 = "A"
+ | x >= 80 && x <= 89 = "B"
+ | x >= 70 && x <= 79 = "C"
+ | x >= 60 && x <= 69 = "D"
+ | x >= 50 && x <= 60 =  "F"
+ | otherwise = "invalid grade"
+ 
+main :: IO ()
+main = do 
+ putStrLn (grade 95) 
+ putStrLn (grade 72) 
+ putStrLn (grade 50)
