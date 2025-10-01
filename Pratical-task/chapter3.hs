d52529a762941b790472b0e48602bb37e6063d1d
Kaisman9746 <97808078+Kaisman9746@users.noreply.github.com>
2025-10-01 10:37:48 +0100

    Update chapter3.hs

--- Pratical-task/chapter3.hs
+++ Pratical-task/chapter3.hs
@@ -4,6 +4,23 @@ Use an if-then-else statement to check if the number is positive, negative, or z
 Return "Positive", "Negative", or "Zero" accordingly.
 Test your function with checkNumber 5, checkNumber (-3), and checkNumber 0.
 
+checkNumber :: Int -> String 
+checkNumber x =
+   if x > 0
+    then "positive"
+       else if x < 0 
+          then "negative"
+             else "zero"
+           --output negative positive zero
+           
+          
+main :: IO ()
+main = do 
+ putStrLn  (checkNumber (-3)) 
+ putStrLn  (checkNumber 5) 
+ putStrLn  (checkNumber 0) 
+
+
 grade :: Int -> String
 grade x = 
  if x >= 90 && x <= 99
