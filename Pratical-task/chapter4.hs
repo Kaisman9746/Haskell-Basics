5fe03967ef50c6bfb1d82c8d9adf74e9d1534e76
Kaisman9746 <97808078+Kaisman9746@users.noreply.github.com>
2025-10-08 13:59:15 +0100

    Update chapter4.hs

--- Pratical-task/chapter4.hs
+++ Pratical-task/chapter4.hs
@@ -23,3 +23,51 @@ main = do
 --It's a bright and beautiful day!
 --Don't forget your umbrella!
 --A bit gloomy, but no rain yet!
+
+
+--using if then else
+
+weatherReport :: String -> String 
+weatherReport w = 
+ if w == "sunny"
+  then "It's a bright and beautiful day!"
+     else if w == "rainy"
+           then "Don't forget your umbrella!"
+              else if w == "cloudy" 
+                    then "A bit gloomy, but no rain yet!"
+                       else "Weather unknown"
+                       
+main :: IO ()
+main = do 
+ putStrLn (weatherReport "rainy") 
+ putStrLn (weatherReport "sunny")
+ putStrLn (weatherReport "cloudy")
+
+--Output
+Don't forget your umbrella!
+It's a bright and beautiful day!
+A bit gloomy, but no rain yet!
+
+    --using guard format
+
+weatherReport :: String -> String 
+weatherReport w
+ | w == "sunny"   = "It's a bright and beautiful day!"
+ | w == "rainy"   =  "Don't forget your umbrella!"
+ | w == "cloudy"  = "A bit gloomy, but no rain yet!"
+ | otherwise    =   "Weather unknown"
+ 
+main :: IO ()
+main = do 
+ putStrLn (weatherReport "sunny") 
+ putStrLn (weatherReport "cloudy") 
+ putStrLn (weatherReport "rainy") 
+ putStrLn (weatherReport "cold") 
+
+
+--Output:
+
+--It's a bright and beautiful day!
+--A bit gloomy, but no rain yet!
+--Don't forget your umbrella!
+Weather unknown 
