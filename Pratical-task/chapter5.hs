24988fdcae3b83d751f1f808cc63ed276da3d0f8
Kaisman9746 <97808078+Kaisman9746@users.noreply.github.com>
2025-10-29 16:30:18 +0100

    Update chapter5.hs

--- Pratical-task/chapter5.hs
+++ Pratical-task/chapter5.hs
@@ -34,3 +34,37 @@ main = do
 0.4
 3.75
 1.0
+
+Use the filter function to extract all odd numbers from a given list of integers from 1 to 30.
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter _ [] = []
+myFilter p (x:xs)
+ | p x        = x : myFilter p xs
+ | otherwise  = myFilter p xs
+ 
+main :: IO ()
+main = print (myFilter odd [1..30])
+
+--Output:
+
+--[1,3,5,7,9,11,13,15,17,19,21,23,25,27,29]
+
+-usinga filter in another form 
+
+filterOdd :: [Int] -> [Int]
+filterOdd = filter odd 
+
+filterEven :: [Int] -> [Int]
+filterEven = filter even 
+
+main :: IO ()
+main = do 
+ print (filterOdd [1..30])
+ print (filterEven [1.. 30])
+
+--Output:
+
+--[1,3,5,7,9,11,13,15,17,19,21,23,25,27,29]
+--[2,4,6,8,10,12,14,16,18,20,22,24,26,28,30]
+
