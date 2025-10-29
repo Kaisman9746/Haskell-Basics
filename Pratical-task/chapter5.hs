a801ab79ee76ef300c571461536ae2011023db30
Kaisman9746 <97808078+Kaisman9746@users.noreply.github.com>
2025-10-29 16:11:53 +0100

    Update chapter5.hs

--- Pratical-task/chapter5.hs
+++ Pratical-task/chapter5.hs
@@ -1,1 +1,20 @@
-5
+--HC5T1: Using applyTwice
+--Define a function that takes a function and --an integer, then applies the function three --times to the integer.
+
+
+--applyThrice :: (Int -> Int) -> Int -> Int
+--applyThrice f x = f (f (f x))
+
+main :: IO ()
+main = do 
+ print $ (applyThrice (+3) 5) 
+ print $ (applyThrice (*2) 3) 
+ print $ (applyThrice (+7) 7) 
+ print $ (applyThrice (*10) 5)
+
+--Output:
+
+14
+24
+28
+5000
