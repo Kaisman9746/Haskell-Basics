921f46fb338b1e41cdc6a45a19509eb2dc76bd71
Kaisman9746 <97808078+Kaisman9746@users.noreply.github.com>
2025-12-04 15:13:41 +0100

    Update Chapter6.hs

--- Pratical-task/Chapter6.hs
+++ Pratical-task/Chapter6.hs
@@ -9,3 +9,27 @@ main :: IO ()
 main = do
     print (factorial 5)
     --output = 120
+    
+--HC6T2: Fibonacci (Recursive)
+--Implement a recursive function to compute the nth Fibonacci number.
+
+ Recursive function to compute the nth Fibonacci number
+fib :: Int -> Int
+fib 0 = 0          -- Base case 1
+fib 1 = 1          -- Base case 2
+fib n = fib (n-1) + fib (n-2)   -- Recursive case
+
+main :: IO ()
+main = do
+    print (fib 10)   -- Example: find the 10th Fibonacci number
+     --Output = 55
+
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+main :: IO ()
+main = do
+    print (fib 7)
+    -output = 13
