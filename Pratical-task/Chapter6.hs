1618869e7aaf66dca82feb1da2048f84ebd28c34
Kaisman9746 <97808078+Kaisman9746@users.noreply.github.com>
2025-12-04 15:05:57 +0100

    Update Chapter6.hs

--- Pratical-task/Chapter6.hs
+++ Pratical-task/Chapter6.hs
@@ -1,1 +1,11 @@
+--HC6T1: Factorial (Recursive)
+--Implement a recursive function to compute the factorial of a numberer.
 
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+main :: IO ()
+main = do
+    print (factorial 5)
+    --output = 120
