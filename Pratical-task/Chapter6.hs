66a358bb241d3c503f5bc96b5ae9429146588f98
Kaisman9746 <97808078+Kaisman9746@users.noreply.github.com>
2025-12-04 15:38:17 +0100

    Update Chapter6.hs

--- Pratical-task/Chapter6.hs
+++ Pratical-task/Chapter6.hs
@@ -33,3 +33,52 @@ main :: IO ()
 main = do
     print (fib 7)
     -output = 13
+    
+--HC6T3: Sum of Elements Using foldr
+--Implement a function to compute the sum of elements in a list using foldr.
+
+sumList :: [Int] -> Int
+sumList xs = foldr (+) 0 xs
+
+main :: IO ()
+main = do
+    print (sumList [1,2,3,4,5])
+     --output = 15
+--HC6T4: Product of Elements Using foldl
+--Implement a function to compute the product of elements in a list using foldl.
+
+productList :: [Int] -> Int
+productList xs = foldl (*) 1 xs
+
+main :: IO ()
+main = do
+    print (productList [1,2,3,4,5])
+     --output = 120
+
+--HC6T5: Reverse a List (Recursive)
+--Implement a function that reverses a list using recursion.
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+main :: IO ()
+main = do
+    print (myReverse [1,2,3,4,5]
+     -- Output = [5,4,3,2,1]
+
+ --HC6T6: Element Exists in List
+--Implement a function that determines whether a given element exists in a list.
+
+
+elementExists :: Eq a => a -> [a] -> Bool
+elementExists _ [] = False
+elementExists y (x:xs)
+    | y == x    = True
+    | otherwise = elementExists y xs
+
+main :: IO ()
+main = do
+    print (elementExists 3 [1,2,3,4,5])  -- True
+    print (elementExists 6 [1,2,3,4,5])  -- False
+     --Output TrueFalse
