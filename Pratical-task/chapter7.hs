c83701803365f0136ac4ea1fb031601092d0b6a5
Kaisman9746 <97808078+Kaisman9746@users.noreply.github.com>
2025-12-05 16:19:20 +0100

    Update chapter7.hs

--- Pratical-task/chapter7.hs
+++ Pratical-task/chapter7.hs
@@ -1,4 +1,4 @@
---HC7T1: Implement an Eq Instance for a Custom Data Type
+it--HC7T1: Implement an Eq Instance for a Custom Data Type
 --Define a data type Color representing Red, Green, and Blue.
 --Implement the Eq type class for it so that colors of the same type are considered equal.
 
@@ -18,3 +18,70 @@ main = do
     print (Green == Green) 
 
    --Output:True False True
+
+--HC7T2: Implement an Ord Instance for a Custom Data Type
+--Using the Color type from HC7T1, implement the Ord type class so that Red < Green < Blue.
+
+data Color = Red | Green | Blue
+
+instance Eq Color where
+    Red   == Red   = True
+    Green == Green = True
+    Blue  == Blue  = True
+    _     == _     = False
+
+instance Ord Color where
+    compare Red   Green = LT
+    compare Red   Blue  = LT
+    compare Green Blue  = LT
+
+    compare Green Red   = GT
+    compare Blue  Red   = GT
+    compare Blue  Green = GT
+
+    compare _ _ = EQ
+
+main :: IO ()
+main = do
+    print (Red < Green)   
+    print (Green < Blue)  
+    print (Blue > Red) 
+    --Output = True True True 
+
+   --HC7T3: Function Using Multiple Constraints
+--Write a function compareValues that takes two arguments of type a and returns the larger one.
+--Ensure that a is both an instance of Eq and Ord 
+
+compareValues :: (Eq a, Ord a) => a -> a -> a
+compareValues x y =
+    if x > y
+       then x
+       else y
+
+main :: IO ()
+main = do
+    print (compareValues 5 3)        
+    print (compareValues 2 10)      
+    print (compareValues 'b' 'a')   
+    --Output: 5 10 'b'
+
+--HC7T4: Custom Type with Show and Read
+--Define a data type Shape with constructors Circle Double and Rectangle Double Double.
+--Implement Show and Read instances for it.
+
+data Shape = Circle Double
+           | Rectangle Double Double
+           deriving (Show, Read)
+
+main :: IO ()
+main = do
+    print (Circle 5)
+    print (Rectangle 3 4)
+
+    print (read "Circle 7" :: Shape)
+    print (read "Rectangle 2 8" :: Shape)
+     --Output:
+     --Circle 5.0
+     --Rectangle 3.0 4.0
+     --Circle 7.0
+     --Rectangle 2.0 8.0
