49325ee533a6ff75ec6da6f33565a5da54e1b95e
Kaisman9746 <97808078+Kaisman9746@users.noreply.github.com>
2025-12-05 15:51:42 +0100

    Update chapter7.hs

--- Pratical-task/chapter7.hs
+++ Pratical-task/chapter7.hs
@@ -1,1 +1,20 @@
-kenny
+--HC7T1: Implement an Eq Instance for a Custom Data Type
+--Define a data type Color representing Red, Green, and Blue.
+--Implement the Eq type class for it so that colors of the same type are considered equal.
+
+
+data Color = Red | Green | Blue
+
+instance Eq Color where
+    Red   == Red   = True
+    Green == Green = True
+    Blue  == Blue  = True
+    _     == _     = False
+
+main :: IO ()
+main = do
+    print (Red == Red)    
+    print (Red == Blue)    
+    print (Green == Green) 
+
+   --Output:True False True
